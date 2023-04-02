23604a8f91a0eebfcef62b24aa82d1730b07b484
Jin <16134123+jaamyang@users.noreply.github.com>
2023-04-02 16:57:39 +0800

    update requirements.txt

--- services/orm/anno_project.py
+++ services/orm/anno_project.py
@@ -18,17 +18,12 @@ metadata_obj = MetaData()
 
 
 class JSONEncodedDict(TypeDecorator):
-  impl = JSON
+    impl = JSON
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
 
-  # def process_bind_param(self, value, dialect):
-  #   if value is not None:
-  #     value = json.dumps(value)
-  #   return value
-
-  def process_result_value(self, value, dialect):
-    if value is not None:
-      value = json.loads(value)
-    return value
 
 project = Table(
     "project",
